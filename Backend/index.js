55c846677c3e5ce494ccccc17e1fe2389951073c
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2024-06-14 19:25:14 +0530

    cercel

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,5 @@
 
-import path from "path";
+
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -22,7 +22,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
+
 
 app.use(express.json({ limit: "5mb" })); // to parse req.body
 // limit shouldn't be too high to prevent DOS
@@ -35,13 +35,6 @@ app.use("/api/users", userRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/Frontend/dist")));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
-	});
-}
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
