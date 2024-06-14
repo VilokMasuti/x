caed2bd611428519b6725450360b28d48a4db9be
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2024-06-15 00:39:51 +0530

    l

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,3 @@
-
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
@@ -24,7 +23,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: "10mb" })); // to parse req.body
+app.use(express.json({ limit: "5mb" })); // to parse req.body
 // limit shouldn't be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
@@ -35,14 +34,14 @@ app.use("/api/users", userRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "frontend", "dist")));
-  
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
 	app.get("*", (req, res) => {
-	    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
-  }
+}
+
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 	connectMongoDB();
