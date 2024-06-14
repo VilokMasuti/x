387561501cb4bfc7902800062507f8cb7e9eff71
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2024-06-15 00:32:51 +0530

    gg

--- Backend/index.js
+++ Backend/index.js
@@ -35,14 +35,14 @@ app.use("/api/users", userRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
+
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/Frontend/dist")));
-
+	app.use(express.static(path.join(__dirname, "frontend", "dist")));
+  
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
+	    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
-}
-
+  }
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 	connectMongoDB();
