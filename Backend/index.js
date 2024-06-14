ca407a2c5b452dfceb9dbc0bd7f1eb5867683602
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2024-06-15 01:09:25 +0530

    ddddd

--- Backend/index.js
+++ Backend/index.js
@@ -14,9 +14,9 @@ import connectMongoDB from "./db/connectMongoDB.js";
 dotenv.config();
 
 cloudinary.config({
-	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const app = express();
@@ -35,14 +35,16 @@ app.use("/api/post", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    // Serve the frontend build files
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+    // Serve the index.html file for all other routes
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
+    });
 }
 
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
-	connectMongoDB();
+    console.log(`Server is running on port ${PORT}`);
+    connectMongoDB();
 });
