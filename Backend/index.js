9418bb261c8c5bf443bf9144a49ca91b76b39c3b
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2024-06-15 19:49:54 +0530

    ccsdc

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,4 @@
+
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
@@ -14,17 +15,19 @@ import connectMongoDB from "./db/connectMongoDB.js";
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.dirname(new URL(import.meta.url).pathname); // Updated
+const __dirname = path.resolve();
 
-app.use(express.json({ limit: "5mb" }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "5mb" })); // to parse req.body
+// limit shouldn't be too high to prevent DOS
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -33,14 +36,14 @@ app.use("/api/post", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "Frontend/dist")));
+	app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
-  });
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
+	});
 }
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  connectMongoDB();
+	console.log(`Server is running on port ${PORT}`);
+	connectMongoDB();
 });
