ffbaee34ddc9e8d2998f2d65434d5621d4ba5c6b
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2024-06-15 11:56:24 +0530

    PACKE FRONEND

--- Backend/index.js
+++ Backend/index.js
@@ -36,11 +36,11 @@ app.use("/api/notifications", notificationRoutes);
 
 if (process.env.NODE_ENV === "production") {
   // Serve the frontend build files
-  app.use(express.static(path.join(__dirname,"/frontend/dist")));
+  app.use(express.static(path.join(__dirname,"/Frontend/dist")));
 
   // Serve the index.html file for all other routes
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
   });
 }
 
