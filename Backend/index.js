fa9d78abf2970db8e2c11f68fbfdb3692e40b188
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2024-06-20 15:38:20 +0530

    speeling

--- Backend/index.js
+++ Backend/index.js
@@ -33,10 +33,10 @@ app.use("/api/users", userRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+	res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
 });
 
 app.listen(PORT, () => {
