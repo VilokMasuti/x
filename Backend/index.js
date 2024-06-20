fc8ab3d0615d04c2436c0c0d263254bd14eed3b3
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2024-06-20 19:42:56 +0530

    env

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,4 @@
-// import path from "path";
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -21,9 +21,9 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
-app.use(express.json({ limit: "5mb" })); // to parse req.body
+app.use(express.json({ limit: "10mb" })); // to parse req.body
 // limit shouldn't be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
@@ -34,13 +34,13 @@ app.use("/api/users", userRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-// 	});
-// }
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
